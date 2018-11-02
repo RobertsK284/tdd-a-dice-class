eb9b60b7288ce0eaf9ef414748a9c70bc6410e7b
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-02 10:39:40 +0000

    passed User story 3 - tests and code for random number between 1 and 6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,9 +2,11 @@ require 'dice'
 describe Dice do
   subject { Dice.new }
   describe '#roll' do
-    it 'responds to roll' do
-    subject.roll
-    expect(subject).to respond_to :roll
+    it 'returns a random number between 1 and 6' do
+      subject.roll
+      expect(subject.roll).to be_an(Integer)
+      expect(subject.roll).to be >= 1
+      expect(subject.roll).to be <= 6
     end
   end
 end
